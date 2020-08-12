598981a403d6786e8e1e7198490c5dab1a3fbaa9
Larissa Souto <larissa@redes.ufsm.br>
2020-08-11 21:07:34 -0300

    initial page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { 
+  MenuItem,
+  FormControl,
+  Select
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <div className="app__header">
+        
+      </div>
+      <h1>COVID-19 TRACKER</h1>
+
+      <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc">
+
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+
+        </Select>
+
+      </FormControl>
     </div>
   );
 }
